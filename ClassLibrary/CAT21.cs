33e3dfbedcd3f00348e927e2c62caa298ff3cbab
Iaguet <iagoamoros@gmail.com>
2021-04-23 11:52:25 +0200

    he hecho un poco, concretmente mirar los frn y coger fixed o variable length data item

--- ClassLibrary/CAT21.cs
+++ ClassLibrary/CAT21.cs
@@ -131,8 +131,198 @@ namespace ClassLibrary
                     byte[] dataItem = utilities.GetFixedLengthDataItem(message, 3);
                 }
             }
-        }
 
+            if (FSPEC.Length >= 3) {
+                if (boolFSPEC[23] == true) //Time of Message Reception of Velocity-High Precision 
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 4);
+
+                }
+
+                if (boolFSPEC[22] == true) //Geometric Height
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 2);
+
+                }
+
+                if (boolFSPEC[21] == true) //Quality Indicators 
+                {
+                    byte[] datItem = utilities.GetVariableLengthDataItem(message);
+
+                }
+
+                if (boolFSPEC[20] == true) //MOPS Version 
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message,1);
+
+                }
+
+                if (boolFSPEC[19] == true) //Mode 3/A Code
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 2);
+
+                }
+
+                if (boolFSPEC[18] == true) //Roll Angle
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 2);
+
+                }
+
+                if (boolFSPEC[17] == true) //Flight Level
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 2);
+
+                }
+
+
+            }
+
+            if (FSPEC.Length >= 4)
+            {
+                if (boolFSPEC[31] == true) //Magnetic heading
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 2);
+
+                }
+
+                if (boolFSPEC[30] == true) //Target status
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 1);
+
+                }
+
+                if (boolFSPEC[29] == true) //Barometric vertical rate
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 2);
+
+                }
+
+                if (boolFSPEC[28] == true) //Geometric vertical rate
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 2);
+
+                }
+
+                if (boolFSPEC[27] == true) //Airborne Ground Vector
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 4);
+
+                }
+
+                if (boolFSPEC[26] == true) //Track angle rate
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 2);
+
+                }
+
+                if (boolFSPEC[25] == true) //Time of Report Transmission
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 3);
+
+                }
+            }
+            if (FSPEC.Length >= 5) {
+                if (boolFSPEC[39] == true) //Target Identification
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 6);
+
+                }
+
+                if (boolFSPEC[38] == true) //Emitter category
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 1);
+
+                }
+
+                if (boolFSPEC[37] == true) //Met information
+                {
+                    byte[] datItem = utilities.GetVariableLengthDataItem(message);
+
+                }
+
+                if (boolFSPEC[36] == true) //Selected altitude
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 2);
+
+                }
+
+                if (boolFSPEC[35] == true) //Final state selected altitude
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 2);
+
+                }
+
+                if (boolFSPEC[34] == true) //Trajectory intent
+                {
+                    byte[] datItem = utilities.GetVariableLengthDataItem(message);
+
+                }
+                if (boolFSPEC[33] == true) //Service management
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 1);
+
+                }
+            }
+
+            if (FSPEC.Length >= 6)
+            {
+                if (boolFSPEC[47] == true) //Aircraft Operational Status
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 1);
+
+                }
+
+                if (boolFSPEC[46] == true) //Surfce capabilities and charcterisitcs
+                {
+                    byte[] datItem = utilities.GetVariableLengthDataItem(message);
+
+
+                }
+
+                if (boolFSPEC[45] == true) //Message Amplitude
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 1);
+
+                }
+
+                if (boolFSPEC[44] == true) //Mode S MB Data
+                {
+
+
+                }
+
+                if (boolFSPEC[43] == true) //ACAS Resolution Advisory Report
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 7);
+
+                }
+
+                if (boolFSPEC[42] == true) //Reciever ID
+                {
+                    byte[] datItem = utilities.GetFixedLengthDataItem(message, 1);
+
+                }
+
+                if (boolFSPEC[41] == true) //Data Ages
+                {
+                    byte[] datItem = utilities.GetVariableLengthDataItem(message);
+
+                }
+            }
+            if (FSPEC.Length >= 7) {
+                    if (boolFSPEC[50] == true) //Reserved expansion field
+                    {
+                        byte[] datItem = utilities.GetVariableLengthDataItem(message);
+
+                    }
+
+                    if (boolFSPEC[49] == true) //Special purpose field
+                    {
+                        byte[] datItem = utilities.GetVariableLengthDataItem(message);
+                    }
+                } 
+            }
         public void DecodeDataSourceIdentifier(byte[] dataItem)
         {
             this.systemIdentificationCode = dataItem[1];
